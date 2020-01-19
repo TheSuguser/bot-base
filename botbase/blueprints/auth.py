d8c1e006afa286573bb884baeb3018e8e513f179
Zhe <joezheng@deloitte.com.cn>
2020-01-19 17:54:30 +0800

    上班时最后一次修改

--- botbase/blueprints/auth.py
+++ botbase/blueprints/auth.py
@@ -5,7 +5,7 @@ auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return 'hello'
+    return render_template('auth/login.html')
 
 @auth_bp.route('logout')
 def logout():
