48ec29f2fac9279dbc494969bdf993b40a6a9642
Zhe <joezheng@deloitte.com.cn>
2020-01-21 13:48:49 +0800

    增加project管理系统，可以删除和新增project

--- botbase/blueprints/front.py
+++ botbase/blueprints/front.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
-from flask import url_for, redirect, render_template
-from flask_login import login_required
+from flask import render_template, flash, redirect, url_for, request, current_app, Blueprint, send_from_directory
+from flask_login import login_required, current_user
+
+from botbase.models import User, Project
+from botbase.forms import ProjectForm
+from botbase.extensions import db
+from botbase.utils import redirect_back
 
 front_bp = Blueprint('front', __name__)
 
@@ -11,4 +16,28 @@ def index():
 @front_bp.route('/panel')
 @login_required
 def panel():
-    return render_template('front/panel.html')
+    projects = Project.query.filter_by(user_id=current_user.id).all()
+    return render_template('front/panel.html', projects=projects)
+
+@front_bp.route('/create_project', methods=['GET', 'POST'])
+@login_required
+def create_project():
+    form = ProjectForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        area = form.area.data 
+        user_id = current_user.id
+        project = Project(name=name, area=area, user_id=current_user.id)
+        db.session.add(project)
+        db.session.commit()
+        return redirect(url_for('front.panel'))
+    return render_template('front/create_project.html', form=form)
+
+@front_bp.route('/project/<int:project_id>/delete', methods=['POST'])
+@login_required
+def delete_project(project_id):
+    project = Project.query.get_or_404(project_id)
+    db.session.delete(project)
+    db.session.commit()
+    flash('Project deleted.', 'success')
+    return redirect_back()
